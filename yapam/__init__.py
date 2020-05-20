ce356c8a846deb74fba1079bddb6947ac3080f14
devalv <63240599+devalv@users.noreply.github.com>
2020-05-21 00:26:55 +0300

    The project structure converted to package-mode.
    README, setup.py and build requirements.

---
+++ yapam/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Yet another ammo generator for Yandex Tank.
+
+for additional info see README.md
+"""
+
+__version__ = '0.1.0'
+__all__ = [
+    'armory', 'config', 'phantom'
+]
+
+__author__ = 'Aleksey Devyatkin <devyatkin.av@ya.ru>'
