48e3cdd04d31e86aa61fcbcb2b7aae4853060b33
Aleksey Devyatkin <devyatkin.av@ya.ru>
2020-05-21 12:26:21 +0300

    README.md links to official docs.

--- yapam/__init__.py
+++ yapam/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-"""Yet another ammo generator for Yandex Tank.
+"""Yet another ammo maker for Yandex Tank.
 
 for additional info see README.md
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __all__ = [
     'armory', 'config', 'phantom'
 ]
