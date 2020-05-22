ded13b4b68c2b402be25267993a5d034cdae7101
Aleksey Devyatkin <devyatkin.av@ya.ru>
2020-05-23 01:06:32 +0300

    Tests updated for backward compatibility with Python 3.5

--- tests/test_phantom.py
+++ tests/test_phantom.py
@@ -8,9 +8,16 @@ from yapam.phantom import PhantomAmmo
 pytestmark = [pytest.mark.phantom]
 
 
+def convert_headers_str_to_dict(headers_str: str) -> dict:
+    """Convert headers in str format to dict."""
+    headers_gen = (header.split(': ') for header in headers_str.split('\r\n'))
+    headers_dict = {header[0]: header[1] for header in headers_gen}
+    return headers_dict
+
+
 @pytest.fixture
 def phantom_ammo_dict(logger):
-    """Fixture with PhantomAmmo parameters without body."""
+    """Phantom ammo parameters without body."""
     ammo_dict = {
         'host': '127.0.0.1',
         'port': 8888,
@@ -26,7 +33,7 @@ def phantom_ammo_dict(logger):
 
 @pytest.fixture
 def phantom_ammo_with_body_dict(phantom_ammo_dict):
-    """Fixture with PhantomAmmo parameters with body."""
+    """Phantom ammo parameters with body."""
     ammo_dict = phantom_ammo_dict
     ammo_dict['method'] = 'POST'
     ammo_dict['body'] = '{\"username\": \"admin\", \"password\": \"admin\"}'
@@ -35,105 +42,169 @@ def phantom_ammo_with_body_dict(phantom_ammo_dict):
 
 
 @pytest.fixture
-def phantom_headers_str():
-    """Fixture with expected default headers converted to str."""
-    headers_str = ('Authorization: token\r\n'
-                   'Host: 127.0.0.1:8888\r\n'
-                   'User-Agent: phantom\r\n'
-                   'Accept: */*\r\n'
-                   'Content-Type: application/json\r\n'
-                   'Connection: Close'
-                   )
-    return headers_str
+def phantom_headers_dict():
+    """Phantom default headers as a dictionary."""
+    headers_dict = {
+        'Authorization': 'token',
+        'Host': '127.0.0.1:8888',
+        'Accept': '*/*',
+        'Content-Type': 'application/json',
+        'Connection': 'Close',
+        'User-Agent': 'phantom'
+    }
+    return headers_dict
 
 
 @pytest.fixture
-def phantom_headers_with_body_str(phantom_headers_str):
-    """Fixture with expected default headers for request with body converted to str."""
-    return '\r\n'.join([phantom_headers_str, 'Content-Length: 42'])
+def phantom_headers_with_body_dict(phantom_headers_dict):
+    """Phantom default headers with additional parameter as a dict."""
+    headers_dict = {'Content-Length': '42'}
+    headers_dict.update(phantom_headers_dict)
+    return headers_dict
 
 
 @pytest.fixture
-def phantom_request_str(phantom_headers_str):
-    """Fixture with expected Phantom request str."""
-    request_str = 'GET /auth HTTP/1.1\r\n' + phantom_headers_str
-    return request_str
+def phantom_request_dict(phantom_headers_dict):
+    """Phantom request parameter as a strange dict."""
+    body_dict = dict()
+    body_dict['1'] = 'GET /auth HTTP/1.1'
+    body_dict['2'] = phantom_headers_dict
+    return body_dict
 
 
 @pytest.fixture
-def phantom_request_with_body_str(phantom_headers_with_body_str):
-    """Fixture with expected Phantom request with body str."""
-    return '\r\n'.join(
-        ['POST /auth HTTP/1.1', phantom_headers_with_body_str, '\r\n{"username": "admin", "password": "admin"}'])
+def phantom_request_with_body_dict(phantom_headers_with_body_dict):
+    """Phantom request as a strange dictionary."""
+    body_dict = dict()
+    body_dict['1'] = 'POST /auth HTTP/1.1'
+    body_dict['2'] = phantom_headers_with_body_dict
+    body_dict['3'] = '{"username": "admin", "password": "admin"}'
+    return body_dict
 
 
 @pytest.fixture
-def phantom_bullet_str(phantom_request_str):
-    """Fixture with expected phantom bullet."""
-    return '147 tests\n' + phantom_request_str + '\r\n\r\n'
+def phantom_bullet_dict(phantom_request_dict):
+    """Phantom bullet property values as a dictionary."""
+    bullet_dict = {'0': '147 tests'}
+    bullet_dict.update(phantom_request_dict)
+    return bullet_dict
 
 
 @pytest.fixture
-def phantom_bullet_with_body_str(phantom_request_with_body_str):
-    """Fixture with expected phantom bullet for request with body."""
-    return '214 tests\n' + phantom_request_with_body_str + '\r\n\r\n'
+def phantom_bullet_with_body_dict(phantom_request_with_body_dict):
+    """Phantom bullet with body property values as a dictionary."""
+    bullet_with_body_dict = {'0': '214 tests'}
+    bullet_with_body_dict.update(phantom_request_with_body_dict)
+    return bullet_with_body_dict
 
 
 @pytest.fixture
 def phantom_ammo_inst(phantom_ammo_dict):
-    """Fixture with PhantomAmmo (without body) instance."""
+    """Phantom ammo (without body) instance."""
     return PhantomAmmo(**phantom_ammo_dict)
 
 
 @pytest.fixture
 def phantom_ammo_with_body_inst(phantom_ammo_with_body_dict):
-    """Fixture with PhantomAmmo (with body) instance."""
+    """Phantom ammo (with body) instance."""
     return PhantomAmmo(**phantom_ammo_with_body_dict)
 
 
 class TestPhantomAmmo:
     """Phantom ammo without body test cases."""
 
+    @staticmethod
+    def convert_request_str_to_dict(request_str: str) -> dict:
+        """Convert request in str format to dict."""
+        str_list = [el for el in request_str.split('\r\n') if el]
+        request_dict = dict()
+        request_dict['1'] = str_list[0]
+        request_dict['2'] = convert_headers_str_to_dict(
+            '\r\n'.join(str_list[1:]))
+        return request_dict
+
+    @staticmethod
+    def convert_bullet_str_to_dict(bullet_str: str) -> dict:
+        """Convert request in str format to dict."""
+        str_list = [el for el in bullet_str.replace('\r', '').split('\n') if el]
+        bullet_dict = dict()
+        bullet_dict['0'] = str_list[0]
+        bullet_dict['1'] = str_list[1]
+        bullet_dict['2'] = convert_headers_str_to_dict(
+            '\r\n'.join([header_str for header_str in str_list[2:] if header_str]))
+        return bullet_dict
+
     def test_init(self, phantom_ammo_dict):
         """Constructor test."""
         assert isinstance(PhantomAmmo(**phantom_ammo_dict), PhantomAmmo)
 
-    def test_headers_property(self, phantom_ammo_inst, phantom_headers_str):
+    def test_headers_property(self, phantom_ammo_inst, phantom_headers_dict):
         """Check that headers property converted as expected."""
-        assert phantom_ammo_inst.headers == phantom_headers_str
+        assert convert_headers_str_to_dict(phantom_ammo_inst.headers) == phantom_headers_dict
 
-    def test_request_property(self, phantom_ammo_inst, phantom_request_str):
+    def test_request_property(self, phantom_ammo_inst, phantom_request_dict):
         """Check that request property converted as expected."""
-        assert phantom_ammo_inst.request == phantom_request_str
+        assert self.convert_request_str_to_dict(phantom_ammo_inst.request) == phantom_request_dict
 
-    def test_bullet_property(self, phantom_ammo_inst, phantom_bullet_str, temporary_log_file):
+    def test_bullet_property(self, phantom_ammo_inst, phantom_bullet_dict, temporary_log_file):
         """Check that bullet property converted as expected."""
-        assert phantom_ammo_inst.bullet == phantom_bullet_str
+        assert self.convert_bullet_str_to_dict(phantom_ammo_inst.bullet) == phantom_bullet_dict
 
         def last_log_line(file_name: str):
             """Return last string from temporary log."""
             with open(file_name, 'r') as log_file:
                 lines = log_file.readlines()
             return lines[-1]
-        for attr in phantom_bullet_str.split('\n'):
-            assert attr.replace('\r', '') in last_log_line(temporary_log_file)
+
+        for val in phantom_bullet_dict.values():
+            if isinstance(val, str):
+                assert val in last_log_line(temporary_log_file)
+            elif isinstance(val, dict):
+                for k, v in val.items():
+                    assert k in last_log_line(temporary_log_file)
+                    assert v in last_log_line(temporary_log_file)
 
 
 class TestPhantomAmmoWithBody:
     """Phantom ammo with body test cases."""
 
+    @staticmethod
+    def convert_request_with_str_with_body_to_dict(request_str: str) -> dict:
+        """Convert request in str format to dict."""
+        str_list = [el for el in request_str.split('\r\n') if el]
+        request_dict = dict()
+        request_dict['1'] = str_list[0]
+        request_dict['2'] = convert_headers_str_to_dict(
+            '\r\n'.join(str_list[1:-1]))
+        request_dict['3'] = str_list[-1]
+        return request_dict
+
+    @staticmethod
+    def convert_bullet_with_body_str_to_dict(bullet_str: str) -> dict:
+        """Convert request in str format to dict."""
+        str_list = [el for el in bullet_str.replace('\r', '').split('\n') if el]
+        bullet_dict = dict()
+        bullet_dict['0'] = str_list[0]
+        bullet_dict['1'] = str_list[1]
+        bullet_dict['2'] = convert_headers_str_to_dict(
+            '\r\n'.join([header_str for header_str in str_list[2:-1] if header_str]))
+        bullet_dict['3'] = str_list[-1]
+        return bullet_dict
+
     def test_init(self, phantom_ammo_with_body_dict):
         """Constructor test."""
         assert isinstance(PhantomAmmo(**phantom_ammo_with_body_dict), PhantomAmmo)
 
-    def test_headers_property(self, phantom_ammo_with_body_inst, phantom_headers_str):
+    def test_headers_property(self, phantom_ammo_with_body_inst, phantom_headers_dict):
         """Check that headers property converted as expected."""
-        assert phantom_ammo_with_body_inst.headers == phantom_headers_str
+        assert convert_headers_str_to_dict(phantom_ammo_with_body_inst.headers) == phantom_headers_dict
 
-    def test_request_property(self, phantom_ammo_with_body_inst, phantom_request_with_body_str):
+    def test_request_property(self, phantom_ammo_with_body_inst, phantom_request_with_body_dict):
         """Check that request property converted as expected."""
-        assert phantom_ammo_with_body_inst.request == phantom_request_with_body_str
+        assert self.convert_request_with_str_with_body_to_dict(
+            phantom_ammo_with_body_inst.request) == phantom_request_with_body_dict
 
-    def test_bullet_property(self, phantom_ammo_with_body_inst, phantom_bullet_with_body_str):
+    def test_bullet_property(self, phantom_ammo_with_body_inst, phantom_bullet_with_body_dict):
         """Check that bullet property converted as expected."""
-        assert phantom_ammo_with_body_inst.bullet == phantom_bullet_with_body_str
+        assert self.convert_bullet_with_body_str_to_dict(
+            phantom_ammo_with_body_inst.bullet) == phantom_bullet_with_body_dict
